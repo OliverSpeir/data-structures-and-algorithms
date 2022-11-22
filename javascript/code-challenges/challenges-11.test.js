81074acd86416f19da49cec825f0a2b5b256cc4f
Oliver Speir <oliverspeir9@gmail.com>
2022-11-22 03:26:41 +0000

    most of chall11

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -19,8 +19,8 @@ Becomes:
 ------------------------------------------------------------------------------------------------ */
 
 function transformToLis(obj){
-  // Solution code here...
-};
+  return Object.keys(obj).map((x)=> '<li>' + x + ': ' + obj[x] + '</li>' );
+}
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -33,7 +33,15 @@ For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
 
 const count = (target, input) => {
-  // Solution code here...
+  let counter = 0;
+  input.map(x => {
+    x.map(y => {
+      if (y===target) {
+        counter++;
+      }
+    });
+  });
+  return counter;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -47,7 +55,13 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
-  // Solution code here...
+  let counter = 0;
+  input.map(x => {
+    x.map(y => {
+      counter = counter + y;
+    });
+  });
+  return counter;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -59,11 +73,19 @@ This function should first remove any elements that are not numbers or are not d
 
 This function should then raise 2 to the power of the resulting numbers, returning an array of arrays.
 
+      if (y === Number && y % 5 === 0) {
+        newArr.push(Math.pow(2,y));
+      }
+
+
 For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
-  // Solution code here...
+  let newArr = input.map (x => {
+    x.filter(y => y === Number && y % 5 === 0);
+  });
+  return newArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -129,7 +151,17 @@ let starWarsData = [{
 }];
 
 let findMaleAndFemale = (data) => {
-  // Solution code here...
+  let str = '';
+  data.map (x => {
+    // console.log (x.gender);
+    if (x.gender === 'male'){
+      str += x.name + ' and ';
+    } if (x.gender === 'female'){
+      str += x.name + ' and ';
+    }
+  });
+  const lastIndexOfSpace = str.lastIndexOf(' ');
+  return str.substring(0,lastIndexOfSpace-4);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -139,7 +171,10 @@ Write a function named findShortest that, given the Star Wars data from Challeng
 ------------------------------------------------------------------------------------------------ */
 
 let findShortest = (data) => {
-  // Solution code here...
+  const min = data.reduce((acc, cur) => {
+    return Math.min(acc.height,cur.height);
+  });
+  console.log(min);
 };
 
 /* ------------------------------------------------------------------------------------------------
