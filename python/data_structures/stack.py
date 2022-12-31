a5c8260fee0794bcd19a996ba3d3de03d34c4d36
Oliver Speir <oliverspeir9@gmail.com>
2022-12-31 16:36:53 -0700

    part of chall10

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,36 @@
+from invalid_operation_error import InvalidOperationError
+
+class Node:
+    def __init__(self, value=None, nextval=None):
+        self.value = value
+        self.next = nextval
+
 class Stack:
     """
     Put docstring here
     """
 
     def __init__(self):
-        # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
-        # method body here
-        pass
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+    def pop(self):
+        if self.top is NOne:
+            raise InvalidOperationError("Method not allowed on empty collection")
+        value = self.top.value
+        self.top = self.top.next
+        return value
+
+    def peek(self):
+        try:
+            return self.top.value
+        except Exception as e:
+            raise InvalidOperationError("Method not allowed one empty collection")
+
+    def is_empty(self):
+        return "self.top is None"
+
