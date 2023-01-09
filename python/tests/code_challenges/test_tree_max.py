8625dff01841f780bdb372ec5d006988ae26dd22
Oliver Speir <oliverspeir9@gmail.com>
2023-01-09 15:33:45 -0700

    CC16

--- python/tests/code_challenges/test_tree_max.py
+++ python/tests/code_challenges/test_tree_max.py
@@ -1,8 +1,7 @@
 import pytest
-from data_structures.binary_tree import BinaryTree, Node
+from python.data_structures.binary_tree import BinaryTree, Node
 
 
-@pytest.mark.skip("TODO")
 def test_max_val():
     tree = BinaryTree()
     tree.root = Node(10)
@@ -12,4 +11,25 @@ def test_max_val():
     actual = tree.find_maximum_value()
     expected = 30
 
+    assert actual == expected
+
+
+def test_max_val_empty():
+    tree = BinaryTree()
+    actual = tree.find_maximum_value()
+    expected = None
+    assert actual == expected
+
+
+def test_max_val_floats_and_negative():
+    tree = BinaryTree()
+    tree.root = Node(1)
+    tree.root.left = Node(45.2)
+    tree.root.right = Node(21.1)
+    tree.root.left.left = Node(9.6)
+    tree.root.left.right = Node(100.1)
+    tree.root.right.left = Node(2)
+    tree.root.right.right = Node(-50)
+    actual = tree.find_maximum_value()
+    expected = 100.1
     assert actual == expected
